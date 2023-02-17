4178ee5e61713c631aac313e5ac862a6053c13f8
JohnIdyu <idyujohncrypto@gmail.com>
2023-02-17 04:30:01 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,21 +1,38 @@
 #!/usr/bin/env python3
 """
-create a web cach
+create a web cache
 """
 import redis
 import requests
 rc = redis.Redis()
-count = 0
-
 
 def get_page(url: str) -> str:
-    """ get a page and cach value"""
-    rc.set(f"cached:{url}", count)
-    resp = requests.get(url)
-    rc.incr(f"count:{url}")
-    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
+    """ get a page and cache value"""
+    try:
+        cached_value = rc.incr(f"cached:{url}")
+    except redis.exceptions.RedisError as e:
+        print(f"Error incrementing cache value: {e}")
+        cached_value = 0
+    else:
+        if cached_value == 1:
+            rc.expire(f"cached:{url}", 10)
+    
+    try:
+        resp = requests.get(url)
+        rc.setex(f"page:{url}", 10, resp.text)
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching page: {e}")
+        resp = ""
+    
     return resp.text
 
 
 if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    urls = [
+        'http://slowwly.robertomurray.co.uk',
+        'http://example.com',
+        'http://google.com',
+    ]
+    
+    for url in urls:
+        print(get_page(url))
